a1a6ae9ef47a94c20e895b6a559b5f10467c12b3
didriksi <didriksten@gmail.com>
2021-10-08 12:16:23 +0200

    Fix issue with missing semicolons

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -5,8 +5,8 @@
 
 Particle::Particle(double charge, double mass, arma::vec position, arma::vec velocity) {
     // require r anv v to be size 3
-    q = charge
-    m = mass
-    r = position
-    v = velocity
+    q = charge;
+    m = mass;
+    r = position;
+    v = velocity;
 }
