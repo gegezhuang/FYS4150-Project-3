9ae45333a9a66c76fa873a43327544a9831f88f8
carlfre <carlfrederiknk@gmail.com>
2021-10-13 03:10:11 +0200

    Implemented Forward Euler and RK4.

--- include/project3/PenningTrap.hpp
+++ include/project3/PenningTrap.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <armadillo>
+#include <vector>
 #include "project3/Particle.hpp"
 
 class PenningTrap {
@@ -14,12 +15,23 @@ class PenningTrap {
         arma::vec total_force_external(int i);
         arma::vec total_force_particles(int i);
         arma::vec total_force(int i);
-        void evolve_RK4(double dt);
-        void evolve_forward_Euler(double dt);
+        //Solves ODE with forward Euler. N is number of points in solution.
+        void solve_forward_Euler(int N, double dt);
+        //Solves ODE with Runge Kutta 4. N is number of points in solution.
+        void solve_RK4(int N, double dt);
+        std::vector<arma::mat> get_solution();
+        std::vector<double> get_time();
     private:
         double B_0;
         double V_0;
         double d;
+        double t;
         std::vector<Particle> particles;
-
+        //solution[k] contains matrix with the solution in t_k.
+        //Column i in the matrix is the position of particle i in t_k.
+        std::vector<arma::mat> solution;
+        std::vector<double> t_sol;
+        //Calculate position of particles in t_(k+1) and add to solution
+        void evolve_RK4(double dt);
+        void evolve_forward_Euler(double dt);
 };
--- src/PenningTrap.cpp
+++ src/PenningTrap.cpp
@@ -15,9 +15,9 @@ void PenningTrap::add_particle(Particle p){
 
 arma::vec PenningTrap::external_E_field(arma::vec r){
     arma::vec E(3);
-    E(0) = - 2 * r(0);
-    E(1) = - 2 * r(1);
-    E(2) = 4 * r(2);
+    E(0) = - 2.0 * r(0);
+    E(1) = - 2.0 * r(1);
+    E(2) = 4.0 * r(2);
     return E;
 }
 
@@ -36,31 +36,143 @@ arma::vec PenningTrap::force_particle(int i, int j){
     return coloumb_constant * p_i.q * p_j.q * (dist) / (abs_dist * abs_dist * abs_dist);
 }
 
-arma::vec total_force_external(int i){
-    return particle.q * (external_E_field(particle.r) 
-                        + arma::cross(particle.v, external_B_field(particle.r));
+arma::vec PenningTrap::total_force_external(int i){
+    return particles[i].q * (external_E_field(particles[i].r) 
+                        + arma::cross(particles[i].v, external_B_field(particles[i].r)));
 }
 
-arma::vec total_force_particles(int i){
-    arma::vec force_particles(3, arma::fill::zeroes);
+arma::vec PenningTrap::total_force_particles(int i){
+    arma::vec force_particles(3, arma::fill::zeros);
     int n = particles.size();
     for (int j = 0; j < n; j++){
-        if i != j{
+        if (i != j){
             force_particles += force_particle(i, j);
         }
     }
     return force_particles;
 }
 
-arma::vec total_force(int i){
+arma::vec PenningTrap::total_force(int i){
     return total_force_particles(i) + total_force_external(i);
 }
 
 void PenningTrap::evolve_forward_Euler(double dt){
     int n = particles.size();
-    Particle particle;
+    arma::vec a_i;
+    arma::mat positions(3, n);
+    arma::mat velocities(3, n);
+    //Calculate v_(i+1), r_(i+1) using forward Euler
     for (int i = 0; i < n; i++){
-        particle = particles[i];
-        a_i = 
+        a_i = total_force(i) / particles[i].m;
+        velocities.col(i) = particles[i].v + dt*a_i;
+        positions.col(i) = particles[i].r + dt*particles[i].v;
     }
+    //add to solution
+    t = t + dt;
+    t_sol.push_back(t);
+    solution.push_back(positions);
+    //update position in Particle objects 
+    for (int i=0; i<n; i++){
+        particles[i].v = velocities.col(i);
+        particles[i].r = positions.col(i);
+    }
+}
+
+void PenningTrap::solve_forward_Euler(int N, double dt){
+    //remove previous solution and reserve memory
+    solution.clear();
+    t_sol.clear();
+    solution.reserve(N);
+    t_sol.reserve(N);
+    //add initial condition to solution
+    t = 0;
+    evolve_forward_Euler(0.0);
+    //solve ODE
+    for (int i=0; i<N-1; i++){
+        evolve_forward_Euler(dt);
+    }
+}
+
+void PenningTrap::evolve_RK4(double dt){
+    int n = particles.size();
+    Particle particle = particles[0];
+    arma::mat positions(3, n);
+    arma::mat velocities(3, n);
+    arma::mat original_positions(3, n);
+    arma::mat original_velocities(3, n);
+    arma::mat k1_v(3, n); arma::mat k2_v(3, n); 
+    arma::mat k3_v(3, n); arma::mat k4_v(3, n);
+    arma::mat k1_r(3, n); arma::mat k2_r(3, n); 
+    arma::mat k3_r(3, n); arma::mat k4_r(3, n);
+    //read positions/velocities of particles at start of timestep
+    for (int i=0; i<n; i++){
+        original_positions.col(i) = particles[i].r;
+        original_velocities.col(i) = particles[i].v;
+    }
+    //calculate k1
+    for (int i=0; i<n; i++){
+        k1_v.col(i) = total_force(i)/particles[i].m;
+    }
+    k1_r = original_velocities;
+    //Set new values for v, r to calculate k2
+    for (int i=0; i<n; i++){
+        particles[i].v = original_velocities.col(i) + dt*k1_v.col(i)/2;
+        particles[i].r = original_positions.col(i) + dt*k1_r.col(i)/2;
+    }
+    for (int i=0; i<n; i++){
+        k2_v.col(i) = total_force(i)/particles[i].m;
+        k2_r.col(i) = particles[i].v;
+    }
+    //Set new values for v, r to calculate k3
+    for (int i=0; i<n; i++){
+        particles[i].v = original_velocities.col(i) + dt*k2_v.col(i)/2;
+        particles[i].r = original_positions.col(i) + dt*k2_r.col(i)/2;
+    }
+    for (int i=0; i<n; i++){
+        k3_v.col(i) = total_force(i)/particles[i].m;
+        k3_r.col(i) = particles[i].v;
+    }
+    //set new values for v, r to calculate k4
+    for (int i=0; i<n; i++){
+        particles[i].v = original_velocities.col(i) + dt*k3_v.col(i);
+        particles[i].r = original_positions.col(i) + dt*k3_r.col(i);
+    }
+    for (int i=0; i<n; i++){
+        k4_v.col(i) = total_force(i)/particles[i].m;
+        k4_r.col(i) = particles[i].v;
+    }
+    //Compute v_(i+1) and r_(i+1) and add to solution-vector
+    velocities = original_velocities + dt*(k1_v + 2*k2_v + 2*k3_v + k4_v)/6;
+    positions = original_positions + dt*(k1_r + 2*k2_r + 2*k3_r + k4_r)/6;
+    t = t + dt;
+    t_sol.push_back(t);
+    solution.push_back(positions);
+    //update positions + velocities in Particle objects
+    for (int i=0; i<n; i++){
+        particles[i].v = velocities.col(i);
+        particles[i].r = positions.col(i);
+    }
+}
+
+void PenningTrap::solve_RK4(int N, double dt){
+    //remove previous solution and reserve memory
+    solution.clear();
+    t_sol.clear();
+    solution.reserve(N);
+    t_sol.reserve(N);
+    //add initial condition to solution
+    t = 0;
+    evolve_RK4(0.0);
+    //solve ODE
+    for (int i=0; i<N-1; i++){
+        evolve_RK4(dt);
+    }
+}
+
+std::vector<arma::mat> PenningTrap::get_solution(){
+    return solution;
+}
+
+std::vector<double> PenningTrap::get_time(){
+    return t_sol;
 }
