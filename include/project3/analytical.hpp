b32e09e5149aefeba72861d68297abeda4b75938
didriksi <didriksten@gmail.com>
2021-10-15 13:53:28 +0200

    Move analytical solution to own module

---
+++ include/project3/analytical.hpp
@@ -0,0 +1,16 @@
+#ifndef __analytical__
+#define __analytical__
+
+#include <armadillo>
+
+std::vector<arma::vec> solve_analytically(arma::vec t, double q, double m, 
+                                          double x0, double z0, double v0, 
+                                          double B0, double V0, double d);
+    // Computes the the analytical solution of a penning trap with one charged 
+    // particle in it, assuming y0 = 0, and the velocity in x and z directions 
+    // are also 0
+
+    // TODO: Consider changing the format so that indexing is the same here as 
+    // for the numerical solution
+
+#endif
---
+++ src/analytical.cpp
@@ -0,0 +1,34 @@
+#include "project3/analytical.hpp"
+
+std::vector<arma::vec> solve_analytically(arma::vec t, double q, double m, 
+                                          double x0, double z0, double v0, 
+                                          double B0, double V0, double d) {
+    // Computes the the analytical solution of a penning trap with one qd 
+    // particle in it, assuming y0 = 0, and the velocity in x and z directions 
+    // are also 0
+
+    arma::vec p = { x0, 0.0, z0 };
+    arma::vec v = { 0.0, v0, 0 };
+
+    double omega0 = q * B0 / m;
+    double omegaz2 = 2 * q * V0 / (m * pow(d, 2));
+
+    double omegap = (omega0 + sqrt(pow(omega0, 2) - 2*omegaz2))/2;
+    double omegam = (omega0 - sqrt(pow(omega0, 2) - 2*omegaz2))/2;
+
+    double Ap = (v0 + omegam * x0)/(omegam - omegap);
+    double Am = - (v0 + omegap * x0)/(omegam - omegap);
+
+    int N = 100; double dt = 1e-4;
+
+    arma::vec x(t.size()), y(t.size()), z(t.size());
+    for (int i = 0; i < t.size(); i++) {
+        x[i] = Ap * cos(- omegap * t[i]) + Am * cos(- omegam * t[i]);
+        y[i] = Ap * sin(- omegap * t[i]) + Am * sin(- omegam * t[i]);
+        z[i] = 0; // TODO: Add solution in z-direction too
+    }
+
+    std::vector<arma::vec> solution = {x, y, z};
+
+    return solution;
+}
