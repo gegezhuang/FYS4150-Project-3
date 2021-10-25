43c54f8e44f7ed28b945d4c4c551a1fa6ac9fe5e
carlfre <carlfrederiknk@gmail.com>
2021-10-25 04:26:55 +0200

    added code to do fine-grained search for resonance

--- src/main.cpp
+++ src/main.cpp
@@ -32,9 +32,9 @@ void simulate_resonance(double f){
 
     double omega_V_start = .2;
     double omega_V_end = 2.5;
-
     double omega_V_stepsize = .02;
     double N = (omega_V_end - omega_V_start) / omega_V_stepsize;
+
     char buffer[50];
     sprintf(buffer, "output/amplitude%f.csv", f);
     string filename = buffer;
@@ -42,7 +42,7 @@ void simulate_resonance(double f){
     outfile.open(filename);
     outfile << "omega_V,particles left" << endl;
 
-    for (int i = 0; i < N; i++){
+    for (int i = 0; i < N+1; i++){
         double omega_V = omega_V_start + omega_V_stepsize * i; 
         PenningTrap pt(1 * 9.65e1, .0025 * 9.65e7, 500., f, omega_V, false);
         pt.add_random_particles(charge, mass, 100);
@@ -50,6 +50,47 @@ void simulate_resonance(double f){
         outfile << omega_V <<"," << pt.count_particles_in_region() << endl;
     }
     outfile.close();
+}
+
+void simulate_resonance_fine_grained(){
+    double mass = 40.78; // mass of CA-ion
+    double charge = 2; // charge of CA-ion
+
+    double omega_V_start = .29;
+    double omega_V_end = .31;
+    double omega_V_stepsize = .002;
+    double N_omega = (omega_V_end - omega_V_start) / omega_V_stepsize;
+    double f = 0.4; //note that we are only running for one amplitude f=0.4
+
+    double T = 500;
+    double h = 1e-2;
+    int N = T/h;
+
+    ofstream outfile1;
+    outfile1.open("output/fine_grained_no_coulomb_interactions.csv");
+    outfile1 << "omega_V,particles left" << endl;
+
+    for (int i=0; i<N_omega+1; i++){
+        double omega_V = omega_V_start + omega_V_stepsize * i; 
+        PenningTrap pt(1 * 9.65e1, .0025 * 9.65e7, 500., f, omega_V, false);
+        pt.add_random_particles(charge, mass, 100);
+        pt.solve_RK4(N, h);
+        outfile1 << omega_V <<"," << pt.count_particles_in_region() << endl;
+    }
+    outfile1.close();
+
+    ofstream outfile2;
+    outfile2.open("output/fine_grained_with_coulomb_interactions.csv");
+    outfile2 << "omega_V,particles left" << endl;
+
+    for (int i=0; i<N_omega+1; i++){
+        double omega_V = omega_V_start + omega_V_stepsize * i; 
+        PenningTrap pt(1 * 9.65e1, .0025 * 9.65e7, 500., f, omega_V, true);
+        pt.add_random_particles(charge, mass, 100);
+        pt.solve_RK4(N, h);
+        outfile2 << omega_V <<"," << pt.count_particles_in_region() << endl;
+    }
+    outfile2.close();
 }
 
 void problem10(){ //todo probably give different name
@@ -64,6 +105,7 @@ int main(int argc, char * argv[]) {
     if (has_flag("-h", argv, argv+argc)) print_help_message();
     if (has_flag("-t", argv, argv+argc)) run_testing();
     if (has_flag("-p10",argv, argv+argc)) problem10();
+    if (has_flag("-fg", argv, argv+argc)) simulate_resonance_fine_grained();
     
     return 0;
 }
